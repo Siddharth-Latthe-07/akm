4129d23440ecbc5011e3f52ac3a803caf24087c0
Siddharth-Latthe-07 <siddharth.22010603@viit.ac.in>
2024-02-13 22:35:05 +0530

    .py file updated

--- test.py
+++ test.py
@@ -3,52 +3,68 @@ import argparse
 import boto3
 
 class SecretsUploader:
-    def __init__(self, folder_path):
-        self.folder_path = folder_path
+    def __init__(self, region):
+        self.region = region
+        self.client = boto3.client('secretsmanager', region_name=region)
 
-    def upload_to_secrets_manager(self, region, secret_name, file_path):
-        client = boto3.client('secretsmanager', region_name=region)
-        
-        with open(file_path, 'r') as file:
-            secret_value = file.read()
-
+    def upload_secret(self, secret_name, secret_value):
         try:
-            response = client.describe_secret(SecretId=secret_name)
+            response = self.client.describe_secret(SecretId=secret_name)
             secret_exists = True
-        except client.exceptions.ResourceNotFoundException:
+        except self.client.exceptions.ResourceNotFoundException:
             secret_exists = False
 
         if secret_exists:
-            response = client.put_secret_value(
+            response = self.client.put_secret_value(
                 SecretId=secret_name,
                 SecretString=secret_value
             )
-            print(f"Updated secret '{secret_name}' in {region}.")
+            print(f"Updated secret '{secret_name}' in {self.region}.")
         else:
-            response = client.create_secret(
+            response = self.client.create_secret(
                 Name=secret_name,
                 SecretString=secret_value
             )
-            print(f"Created new secret '{secret_name}' in {region}.")
+            print(f"Created new secret '{secret_name}' in {self.region}.")
+
+class FolderSyncer:
+    def __init__(self, folder_path):
+        self.folder_path = folder_path
 
     def sync_folder_to_secrets_manager(self):
         for root, dirs, files in os.walk(self.folder_path):
             for file in files:
                 file_path = os.path.join(root, file)
+                # Extract the region from the folder path
                 folder_parts = root.split('/')
-                region = folder_parts[-1]  # Last part is the region
+                if len(folder_parts) >= 2:
+                    folder_region = folder_parts[-2]  # Second to last part is the region
+                else:
+                    folder_region = ''
                 application_name = os.path.basename(root)
                 secret_name = f"{application_name}/{file}"
-                self.upload_to_secrets_manager(region, secret_name, file_path)
+
+                # If the file path contains a region name, use it, otherwise upload to all specified regions
+                if folder_region:
+                    uploader = SecretsUploader(folder_region)
+                    uploader.upload_secret(secret_name, self.read_file(file_path))
+                else:
+                    regions = [d for d in os.listdir(self.folder_path) if os.path.isdir(os.path.join(self.folder_path, d))]
+                    for region in regions:
+                        uploader = SecretsUploader(region)
+                        uploader.upload_secret(secret_name, self.read_file(file_path))
+
+    def read_file(self, file_path):
+        with open(file_path, 'r') as file:
+            return file.read()
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("folder_path", help="Path to the folder to sync with AWS Secrets Manager")
+    parser = argparse.ArgumentParser(description='Upload files as secrets to AWS Secrets Manager.')
+    parser.add_argument('folder_path', metavar='folder_path', type=str, help='Path to the folder containing files to upload as secrets')
     args = parser.parse_args()
 
-    uploader = SecretsUploader(args.folder_path)
-    uploader.sync_folder_to_secrets_manager()
+    folder_syncer = FolderSyncer(args.folder_path)
+    folder_syncer.sync_folder_to_secrets_manager()
 
 if __name__ == "__main__":
     main()
-
